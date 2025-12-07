791d6e08e0201fe7bcdff27eb889f55e4c32bde6
Parsa Hashemkhany <parsahashemkhany.1385.pppp@gmail.com>
2025-12-07 11:42:51 -0800

    model tests moved to tests

--- models/linear_regression.py
+++ models/linear_regression.py
@@ -1,6 +1,3 @@
-from sklearn.datasets import make_regression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, mean_absolute_error
 import numpy as np
 
 
@@ -39,25 +36,3 @@ class LinearRegressionSGD:
 
     def predict(self, X):
         return X @ self.w + self.b
-
-
-if __name__ == '__main__':
-    X, y = make_regression(n_samples=100, n_features=5, noise=15, random_state=42)
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-
-    lr = LinearRegressionSGD(
-        n_iter=5000,
-        alpha=0.001,
-        random_state=42,
-        learning_rate=0.001
-    )
-    lr.fit(X_train, y_train)
-
-    y_pred = lr.predict(X_test)
-
-    mse = mean_squared_error(y_test, y_pred)
-    mae = mean_absolute_error(y_test, y_pred)
-
-    print('MSE:', mse)
-    print('MAE:', mae)
--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -1,6 +1,3 @@
-from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
 import numpy as np
 
 
@@ -54,22 +51,3 @@ class LogisticRegressionMBGD:
         sigmoid = 1 / (1 + np.exp(-z))
 
         return (sigmoid >= 0.5).astype(int)
-
-
-if __name__ == '__main__':
-    X, y = make_classification(
-        n_samples=5000,
-        n_features=20,
-        n_informative=5,
-        n_redundant=2,
-        n_classes=2,
-        random_state=42
-    )
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
-
-    lr = LogisticRegressionMBGD()
-    lr.fit(X_train, y_train)
-
-    y_pred = lr.predict(X_test)
-    print(classification_report(y_test, y_pred))
